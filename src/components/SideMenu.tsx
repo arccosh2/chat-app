e8a8cf2fd69503464ed614d94f44e4ce3ba1c103
Haruna Nishita <h.nishita.1@gmail.com>
2025-03-02 16:10:20 +0900

    add: サイドメニューにチャットエリアを作成

--- src/components/SideMenu.tsx
+++ src/components/SideMenu.tsx
@@ -1,16 +1,34 @@
 import React from 'react';
 import AIAvatar from '@/components/AIAvatar';
+import { routes } from '@/constants/routes';
+import Link from 'next/link';
+import { cn } from '@/lib/utils';
 
 const SideMenu = () => {
   return (
-    <div className="bg-stone-900 text-stone-200 h-full p-4">
+    <div className="bg-stone-900 text-stone-200 h-full p-4 space-y-3">
       <div className="flex text-stone-300 items-center">
         <div className="mr-4">
           <AIAvatar />
         </div>
-        <h1 className="text-xl font-bold tracking-wider">Chat App</h1>
+        <h1 className="text-2xl font-bold tracking-wider">Chat App</h1>
       </div>
-      <div>chat area</div>
+
+      <div>
+        {routes.map((route) => (
+          <Link
+            href={route.path}
+            key={route.label}
+            className="block p-3 text-xl font-medium tracking-wide text-stone-100/50 hover:text-stone-200 hover:bg-stone-100/10 transition rounded-sm"
+          >
+            <div className="flex items-center">
+              <route.Icon className={cn('w-5 h-5 mr-2', route.iconColor)} />
+              <p>{route.label}</p>
+            </div>
+          </Link>
+        ))}
+      </div>
+
       <div>settings</div>
     </div>
   );
