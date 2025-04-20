f840f511005282012047310aa5f8065e5cfbf6a1
Haruna Nishita <h.nishita.1@gmail.com>
2025-04-20 20:20:32 +0900

    add: ユーザーアイコンにログアウト用のドロップメニューを追加

--- src/components/UserIcon.tsx
+++ src/components/UserIcon.tsx
@@ -3,16 +3,34 @@
 import React from 'react';
 import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar';
 import { useAppSelector } from '@/lib/hooks';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuTrigger,
+} from './ui/dropdown-menu';
 
 const UserIcon = () => {
+  const userName = useAppSelector(
+    (state) => state.auth.currentUser?.displayName
+  );
   const photoURL = useAppSelector((state) => state.auth.currentUser?.photoURL);
   const profileImage = photoURL ?? undefined;
 
   return (
-    <Avatar>
-      <AvatarImage src={profileImage} />
-      <AvatarFallback>CN</AvatarFallback>
-    </Avatar>
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Avatar>
+          <AvatarImage src={profileImage} />
+          <AvatarFallback>CN</AvatarFallback>
+        </Avatar>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="mr-3">
+        <DropdownMenuLabel>{userName}</DropdownMenuLabel>
+        <DropdownMenuItem>ログアウト</DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
   );
 };
 
