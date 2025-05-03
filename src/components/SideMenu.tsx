9b6dab85d7fe22f5b61d1e23a2c53469db0ece99
Haruna Nishita <h.nishita.1@gmail.com>
2025-05-03 12:08:13 +0900

    update: firebaseからチャットのデータを取得

--- src/components/SideMenu.tsx
+++ src/components/SideMenu.tsx
@@ -1,8 +1,8 @@
 'use client';
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import AIAvatar from '@/components/AIAvatar';
-import { routes } from '@/constants/routes';
+import { menus } from '@/constants/menus';
 import Link from 'next/link';
 import { cn } from '@/lib/utils';
 import { usePathname } from 'next/navigation';
@@ -13,9 +13,52 @@ import {
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from './ui/dropdown-menu';
+import {
+  collection,
+  query,
+  where,
+  onSnapshot,
+  orderBy,
+  Timestamp,
+} from 'firebase/firestore';
+import { db } from '@/lib/firebase/firebaseClient';
+import { useAppSelector } from '@/lib/hooks';
+
+type ChatType = 'chat';
+
+interface ChatRoom {
+  id: string;
+  user_id: string;
+  type: ChatType;
+  first_message: string;
+  last_updated: Timestamp;
+}
 
 const SideMenu = () => {
   const pathName = usePathname();
+  const currentUser = useAppSelector((state) => state.auth.currentUser);
+  const [chatRooms, setChatRooms] = useState<ChatRoom[]>([]);
+
+  useEffect(() => {
+    const q = query(
+      collection(db, 'chats'),
+      where('user_id', '==', currentUser?.uid),
+      orderBy('last_updated', 'desc')
+    );
+    const unsubscribe = onSnapshot(q, (querySnapshot) => {
+      const fetchChatRooms = querySnapshot.docs.map((doc) => ({
+        id: doc.id,
+        user_id: doc.data().user_id,
+        type: doc.data().type,
+        first_message: doc.data().first_message,
+        last_updated: doc.data().last_updated,
+      }));
+
+      setChatRooms(fetchChatRooms);
+    });
+
+    return () => unsubscribe();
+  }, [currentUser?.uid]);
 
   return (
     <div className="bg-stone-900 text-stone-200 h-full p-4 space-y-3 flex flex-col">
@@ -27,18 +70,18 @@ const SideMenu = () => {
       </Link>
 
       <div>
-        {routes.map((route) => (
+        {menus.map((menu) => (
           <Link
-            href={route.path}
-            key={route.label}
+            href={menu.path}
+            key={menu.label}
             className={cn(
               'block p-3 text-xl font-medium tracking-wide text-stone-200 hover:bg-stone-100/10 transition rounded-sm',
-              pathName.startsWith(route.path) && 'bg-stone-100/10'
+              pathName.startsWith(menu.path) && 'bg-stone-100/10'
             )}
           >
             <div className="flex items-center">
-              <route.Icon className={cn('w-5 h-5 mr-2', 'text-yellow-600')} />
-              <p>{route.label}</p>
+              <menu.Icon className={cn('w-5 h-5 mr-2', 'text-yellow-600')} />
+              <p>{menu.label}</p>
             </div>
           </Link>
         ))}
@@ -47,24 +90,29 @@ const SideMenu = () => {
       <div className="flex flex-1 flex-col overflow-hidden space-y-1">
         <h2 className="text-xl font-bold px-2 py-3">Threads</h2>
         <div className="overflow-auto">
-          <Link
-            href={'#'}
-            className={cn(
-              'block p-4 text-xl font-medium tracking-wide text-stone-200 hover:bg-stone-100/10 transition rounded-sm'
-            )}
-          >
-            <div className="flex items-center justify-between">
-              <p className="truncate">Message</p>
-              <DropdownMenu>
-                <DropdownMenuTrigger>
-                  <Ellipsis size={16} />
-                </DropdownMenuTrigger>
-                <DropdownMenuContent>
-                  <DropdownMenuItem>削除</DropdownMenuItem>
-                </DropdownMenuContent>
-              </DropdownMenu>
-            </div>
-          </Link>
+          {chatRooms.map((chatRoom) => (
+            <Link
+              key={chatRoom.id}
+              href={`/${chatRoom.type}/${chatRoom.id}`}
+              className={cn(
+                'block py-2 px-4  text-xl font-medium tracking-wide text-stone-200 hover:bg-stone-100/10 transition rounded-sm',
+                pathName === `/${chatRoom.type}/${chatRoom.id}` &&
+                  'bg-stone-100/10'
+              )}
+            >
+              <div className="flex items-center justify-between">
+                <p className="truncate">{chatRoom.first_message}</p>
+                <DropdownMenu>
+                  <DropdownMenuTrigger>
+                    <Ellipsis size={16} />
+                  </DropdownMenuTrigger>
+                  <DropdownMenuContent>
+                    <DropdownMenuItem>削除</DropdownMenuItem>
+                  </DropdownMenuContent>
+                </DropdownMenu>
+              </div>
+            </Link>
+          ))}
         </div>
       </div>
 
