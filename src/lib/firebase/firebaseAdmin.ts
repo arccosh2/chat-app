68a952f7b834e22bfabb3eec65c2609168d86255
Haruna Nishita <h.nishita.1@gmail.com>
2025-05-06 11:03:36 +0900

    add; storageの追加

--- src/lib/firebase/firebaseAdmin.ts
+++ src/lib/firebase/firebaseAdmin.ts
@@ -1,10 +1,22 @@
-import { applicationDefault, initializeApp } from 'firebase-admin/app';
-import { getApp, getApps } from 'firebase/app';
+import {
+  applicationDefault,
+  initializeApp,
+  getApp,
+  getApps,
+} from 'firebase-admin/app';
+import { getFirestore } from 'firebase-admin/firestore';
+import { getStorage } from 'firebase-admin/storage';
 
 if (!getApps().length) {
   initializeApp({
     credential: applicationDefault(),
+    storageBucket: 'chat-app-ba0be.firebasestorage.app',
   });
 } else {
   getApp();
 }
+
+const db = getFirestore();
+const bucket = getStorage().bucket();
+
+export { db, bucket };
